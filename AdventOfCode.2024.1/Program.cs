15fa3a0a275855de59a4f26ff8bf6b57c9ea1602
Drew Crabb <Drew.Crabb@publix.com>
2024-12-02 22:00:37 -0500

    initial

---
+++ AdventOfCode.2024.1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var group1List = (List<int>)[1,2,3];
+var group2List = (List<int>)[2,3,4];
+
+group1List.Sort();
+group2List.Sort();
+
+var distanceSum = 0;
+for (var i = 0; i < group1List.Count; i++)
+{
+    distanceSum += CalculateDistance(group1List[i], group2List[i]);
+}
+
+Console.WriteLine(distanceSum);
+Console.ReadKey();
+
+return;
+
+int CalculateDistance(int x1, int y1) =>
+    Math.Abs(y1-x1);

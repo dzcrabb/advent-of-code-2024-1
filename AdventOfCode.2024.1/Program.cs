c2413beaab18649131ac643d6c8494d72cfe5e40
dzcrabb <dzcrabb@gmail.com>
2024-12-02 22:20:36 -0500

    finish

--- AdventOfCode.2024.1/Program.cs
+++ AdventOfCode.2024.1/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var group1List = (List<int>)[1,2,3];
-var group2List = (List<int>)[2,3,4];
+var (group1List, group2List) = ReadListsFromFile();
 
 group1List.Sort();
 group2List.Sort();
@@ -19,3 +18,22 @@ return;
 
 int CalculateDistance(int x1, int y1) =>
     Math.Abs(y1-x1);
+
+
+(List<int>, List<int>) ReadListsFromFile()
+{
+    var list1 = (List<int>) [];
+    var list2 = (List<int>) [];
+    
+    using var file = File.OpenRead("input.txt");
+    using var reader = new StreamReader(file);
+    while (!reader.EndOfStream)
+    {
+        var line = reader.ReadLine();
+        var values = line!.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        list1.Add(int.Parse(values[0]));
+        list2.Add(int.Parse(values[1]));
+    }
+    
+    return (list1, list2);
+}    
